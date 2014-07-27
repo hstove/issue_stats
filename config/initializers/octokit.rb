57c46122690e3385e409bbbe7c7da17c60d5e7dd
Hank Stoever <hstove@gmail.com>
2014-07-27 15:21:57 -0700

    don't cache github requests

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -3,9 +3,7 @@ module GH
   class << self
     def method_missing method, *args
       key = *args
-      Rails.cache.fetch(key, expires_at: 1.hour.from_now) do
-        client.send method, *args
-      end
+      client.send method, *args
     end
     def client
       @client ||= Octokit::Client.new access_token: ENV['PRWATCH_GITHUB_KEY']
