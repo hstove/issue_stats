766099a9d7693af07598ee0cb4091f85b8131ae6
Hank Stoever <hstove@gmail.com>
2014-07-27 20:08:36 -0700

    omit close_time == 0

--- app/models/report.rb
+++ app/models/report.rb
@@ -8,7 +8,7 @@ class Report < ActiveRecord::Base
   before_create :fetch_metadata
   after_create :bootstrap_async
 
-  scope :ready, -> { where("pr_close_time is not null and issues_count > 25") }
+  scope :ready, -> { where("pr_close_time > 0 and issue_close_time > 0 and issues_count > 25") }
 
   class << self
     def from_key key
