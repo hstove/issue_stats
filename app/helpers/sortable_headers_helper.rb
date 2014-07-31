206302367ecd3482f2b9db190f7a25589b9c4bf6
Hank Stoever <hstove@gmail.com>
2014-07-31 07:22:10 -0700

    refactor default_sortable_opts

--- app/helpers/sortable_headers_helper.rb
+++ app/helpers/sortable_headers_helper.rb
@@ -1,16 +1,6 @@
 module SortableHeadersHelper
   def sortable_header(attr, opts={})
-    opts.symbolize_keys!
-    opts[:attr] = attr
-    opts[:title] ||= attr.to_s.titleize
-    opts[:link_opts] ||= {}
-    opts[:link_opts].merge class: "sortable-header"
-    opts[:is_sorted] = params[:sortable_attr] == attr.to_s
-    url = request.env['PATH_INFO']
-    opts[:path] = Rails.application.routes.recognize_path(url)
-    opts[:path].merge! params
-    opts[:path].delete("page")
-    opts[:path][:sortable_attr] = attr
+    opts = default_sortable_opts(opts, attr)
     current_direction = params[:sortable_direction]
     if opts[:is_sorted]
       opts[:sort_symbol] = sort_symbols[current_direction]
@@ -25,6 +15,21 @@ module SortableHeadersHelper
   end
 
   private
+
+  def default_sortable_opts(opts, attr)
+    opts.symbolize_keys!
+    opts[:attr] = attr
+    opts[:title] ||= attr.to_s.titleize
+    opts[:link_opts] ||= {}
+    opts[:link_opts].merge class: "sortable-header"
+    opts[:is_sorted] = params[:sortable_attr] == attr.to_s
+    url = request.env['PATH_INFO']
+    opts[:path] = Rails.application.routes.recognize_path(url)
+    opts[:path].merge! params
+    opts[:path].delete("page")
+    opts[:path][:sortable_attr] = attr
+    opts
+  end
 
   def sort_symbols
     {
