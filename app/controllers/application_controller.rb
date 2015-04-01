993a3afb5f229c9c24aab82a7b18f8654fdc6b52
hank <hstove@gmail.com>
2015-04-01 11:54:03 -0700

    include MimeResponders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  include ActionController::MimeResponds
+  include ActionController::ImplicitRender
+
   protect_from_forgery with: :exception
 
   helper_method :report_path
